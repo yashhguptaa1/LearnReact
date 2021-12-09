2f30144ba7807629e9ecff23537ad88659b8b037
Yash Gupta <yashhguptaa1@gmail.com>
2021-12-10 02:29:07 +0530

    using axios and map over list

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './1-Props/App';
+import App from './2-Axios-List/App';
 
 ReactDOM.render(
   <React.StrictMode>
